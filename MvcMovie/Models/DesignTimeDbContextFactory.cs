7bfa58fa0549917cc520e813e8532d6960746753
salvatoresciuto <salvo524@hotmail.it>
2018-07-28 00:14:39 +0200

    MvcMoviAppliacation

---
+++ MvcMovie/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MvcMovie.Models;
+
+namespace MvcMovie
+{
+   
+
+    public class MvcMovieDbContextFactory : IDesignTimeDbContextFactory<MvcMovieContext>
+    {
+        public MvcMovieContext CreateDbContext(string[] args)
+        {
+            var connection = "Server=localhost,1433;Database=MvcMovie;User ID=SA;Password='Sal1234!';";
+            var builder = new DbContextOptionsBuilder<MvcMovieContext>();
+            builder.UseSqlServer(connection,
+                                 optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(MvcMovieContext).GetTypeInfo().Assembly.GetName().Name));
+
+            return new MvcMovieContext(builder.Options);
+        }
+    }
+}
---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+namespace MvcMovie.Models{
+    public class MovieGenreViewModel{
+
+        public List<Movie> movies; // pass list of movie model
+        public SelectList genres; //list of genre
+        public string movieGenre { get; set; } //selected genre, this value will set for search
+        
+    }
+}
